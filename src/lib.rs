cca962c9b88ec4a2811d7e488eb406812aa6f6a9
Tim Gion <movestill@gmail.com>
2020-04-19 09:36:53 -0400

    Move modules to lib crate.
    Can't import modules from a binary crate for integration tests so move
    everything into the lib crate.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+
+#[macro_use]
+extern crate diesel;
+
+#[macro_use]
+extern crate diesel_migrations;
+
+pub mod config;
+pub mod data_manager;
+pub mod db;
+pub mod intern;
+pub mod usage_manager;
